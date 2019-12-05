2bb6b1eb73b8b5be96f23036ab14dd719c390a4c
Samuel Allen <S.Allen@reading.ac.uk>
2019-12-05 14:53:25 +0000

    added Da1

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpaceCraft craft = new SpaceCraft();
+            List<int> weights = craft.GetWeights();
+            List<int> moduleFuel = craft.CalculateFuel(weights);
+            int totalWeight = craft.AddFuel(moduleFuel);
+            Console.WriteLine(totalWeight.ToString());
+        }
+    }
+}
---
+++ Day1/SpaceCraft.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day1
+{
+    public class SpaceCraft
+    {
+        public List<int> GetWeights()
+        {
+            List<int> modules = new List<int>();
+            FileStream fileStream = new FileStream("Inputs\\Modules.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (int.TryParse(line, out int weight))
+                    {
+                        modules.Add(weight);
+                    }
+                }
+            }
+            return modules;
+        }
+
+        public List<int> CalculateFuel(List<int> weights)
+        {
+            List<int> fuelValues = new List<int>();
+            foreach(int mass in weights)
+            {
+                decimal fuel = mass / 3;
+                fuel = Math.Floor(fuel) - 2;
+                fuelValues.Add((int)fuel);
+            }
+            return fuelValues;
+        }
+
+        public int AddFuel(List<int> fuelValues)
+        {
+            return fuelValues.Sum();
+        }
+    }
+}
